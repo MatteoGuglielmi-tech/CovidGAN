d268920d4f334f8c8d2667962e1e250234ac1017
Matteo-tech <matteo.guglielmi.ma.gu@gmail.com>
2023-07-13 14:59:33 +0200

    refactor: removed folders for rearrangement

--- src/pytorch-sanity-check.py
+++
@@ -1,7 +0,0 @@
-"""Scanity check ti veryfy that pytorch is installed correctly."""
-import torch
-import torchvision
-
-print("PyTorch Version: ", torch.__version__)
-print("Torchvision Version: ", torchvision.__version__)
-print("CUDA Available: ", torch.cuda.is_available())
